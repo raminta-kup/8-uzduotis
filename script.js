e3f9fac4e86d3b22f86cc429e8f40dac1b176a89
raminta kupcinskaite <raminta.kupcinskaite@gmail.com>
2023-02-20 19:09:32 +0200

    function and testing

--- script.js
+++ script.js
@@ -6,5 +6,24 @@ multiplication() - priima du skaičius ir grąžina jų daugybos rezultatą;
 division() - priima du skaičius ir grąžina jų dalybos rezultatą;
 ------------------------------------------------------------------------------------ */
 
+function Calculator() {
+    this.sum = function (num1, num2) {
+        return num1 + num2;
+    }
+    this.subtraction = function (num1, num2) {
+        return num1 - num2;
+    }
+    this.multiplaction = function (num1, num2) {
+        return num1 * num2;
+    }
+    this.division = function (num1, num2) {
+        return num1 / num2;
+    }
+}
 
+const Result = new Calculator();
+console.log(Result.sum(4, 2));
+console.log(Result.subtraction(4, 2));
+console.log(Result.multiplaction(4, 2));
+console.log(Result.division(4, 2));
 
